657df0ff59dd54e04744216ff3fdb4c2658ceb3f
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-20 19:10:59 +0530

    Dark mode text color

--- layout/Footer.js
+++ layout/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
           <div>
-            <p>&copy; Prajwal Kulkarni. All Rights Reserved.</p>
+            <p className="dark:text-white">&copy; Prajwal Kulkarni. All Rights Reserved.</p>
           </div>
           
           <div className="space-x-3 flex flex-row items-center">
