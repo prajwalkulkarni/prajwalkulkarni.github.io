cfe2985e9e220d6f70e3c5d2cfe36480beb32b94
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-20 19:05:43 +0530

    Dark mode title

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -3,30 +3,24 @@ import userData from "../constants/data";
 
 export default function AboutMe() {
   return (
-    <section className="bg-white dark:bg-gray-800">
+    <section className="bg-white dark:bg-gray-800 w-full">
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+        <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left dark:text-white">
           About Me.
         </h1>
       </div>
       <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
         <div className="text-container max-w-6xl mx-auto pt-20">
           <p
-            className="leading-loose text-2xl md:text-4xl font-semibold  mx-4"
+            className="leading-loose text-2xl md:text-4xl font-semibold  mx-4 dark:text-white"
             style={{ lineHeight: "3rem" }}
           >
-            {userData.about.title}. Currently working on{" "}
-            <a
-              className="bg-red-500 rounded-md px-2 py-1 text-white"
-              href={userData.about.currentProjectUrl}
-            >
-              {userData.about.currentProject} ✈️
-            </a>
+            {userData.about.title}
           </p>
         </div>
       </div>
       <div className="bg-[#F1F1F1] dark:bg-gray-900 px-4">
-        <div className="pt-20 grid grid-cols-1 md:grid-cols-2 max-w-6xl mx-auto gap-y-20 gap-x-20">
+        <div className="pt-20 grid grid-cols-1 gap-x-20 md:grid-cols-2 max-w-6xl mx-auto gap-y-20 gap-x-20">
           {/* Social Buttons */}
         
           {/* Text area col-span-1 md:col-span-2 */}
@@ -45,7 +39,7 @@ export default function AboutMe() {
           <div>
             <div className="flex flex-col">
               <h1 className="bg-red-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">
-                Tech Stack/Languages/Frameworks
+                Tech Stack/Languages
               </h1>
               <div className="flex flex-row flex-wrap mt-8">
                 <img
