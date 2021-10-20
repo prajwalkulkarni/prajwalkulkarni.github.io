e4a65851c393759a3bdb5ae6d4b9efa1f22f8145
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-20 19:08:01 +0530

    Dark mode title

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,19 +1,19 @@
 import userData from "../constants/data";
 import React from "react";
-
+import styles from './Timeline.module.css'
 export default function Timeline() {
   return (
-    <section className="bg-white dark:bg-gray-800">
+    <section className="bg-white dark:bg-gray-800 overflow-x-hidden">
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left dark:text-white">
           Timeline
         </h1>
       </div>
-      <div className="bg-[#F1F1F1] dark:bg-gray-900 -mt-4">
-        <div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto pt-20">
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 ">
+        <div className={`md:flex w-screen`}>
           {/* Experience card */}
           {userData.experience.map((exp, idx) => (
-            <>
+            <div className={`md:flex flex-none m-3 ${styles.scrollBg}`}>
               <TimelineCard
                 key={idx}
                 title={exp.title}
@@ -21,16 +21,10 @@ export default function Timeline() {
                 year={exp.year}
                 company={exp.company}
                 companyLink={exp.companyLink}
+                
               />
-              {idx === userData.experience.length - 1 ? null : (
-                <div className="divider-container flex flex-col items-center -mt-2">
-                  <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
-                    <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
-                  </div>
-                  <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
-                </div>
-              )}
-            </>
+              
+            </div>
           ))}
         </div>
       </div>
@@ -39,7 +33,7 @@ export default function Timeline() {
 }
 
 const TimelineCard = ({ title, desc, year, company, companyLink }) => {
-  return (
+  {/*return (
     <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
       <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
         {year}
@@ -50,5 +44,35 @@ const TimelineCard = ({ title, desc, year, company, companyLink }) => {
       </a>
       <p className="text-gray-600 dark:text-gray-400 my-2">{desc}</p>
     </div>
-  );
+  );*/}
+  return (
+    <div
+      className={`md:card-container flex flex-col justify-between shadow-lg max-w-lg min-w-lg rounded-b-md rounded-t-md `}>
+      <div className="top-text p-8">
+        <div className="icon-quotes">
+          <h1 className="text-4xl text-gray-200 font-bold dark:text-gray-800">
+            {title}
+          </h1>
+          <a href={companyLink} target="_blank" className="text-gray-500">
+            {company}
+          </a>
+        </div>
+        <div className="container-text max-w-lg">
+          <p className="font-semibold text-gray-600 dark:text-gray-400 text-xl py-8">
+            {desc}
+          </p>
+        </div>
+      </div>
+      <div className="footer-container flex flex-row flex-nowrap justify-between min-w-lg max-w-lg bg-gradient-to-r from-green-400 to-blue-500 py-4 px-8 rounded-b-lg">
+        <div className="avatar-container">
+          <div className="footer-img flex flex-row flex-nowrap">
+
+            <div className="footer-text flex flex-col flex-nowrap">
+              <p className="text-gray-100 font-bold ml-6">{year}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
