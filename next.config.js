634d4a9ffc04ea514c5906962b87d360d551b513
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-22 09:34:45 +0530

    Add CDN

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com'],
   },
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
 }
