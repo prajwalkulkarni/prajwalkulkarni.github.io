81ce26e871565dfa8b79d89ef6504abd6c553748
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-10-22 00:04:51 +0530

    Update About Me

--- constants/data.js
+++ constants/data.js
@@ -1,135 +1,132 @@
 const userData = {
-    githubUsername: "prajwalkulkarni",
-    name: "Prajwal Kulkarni",
-    designation: "Frontend Developer",
-    avatarUrl: "/assets/images/avatar.jpg",
-    email: "prajwalkulkarni76@gmail.com",
-    phone: "+91 8105976325",
-    address: "Bengaluru, Karnataka, India.",
-    projects: [
-      {
-        title: "Splitster",
-        link: "https://play.google.com/store/apps/details?id=com.app.splitster&hl=en_IN&gl=US",
-        imgUrl: "/assets/images/splitster.png",
-      },
-      // {
-      //   title: "Textual",
-      //   link: "https://play.google.com/store/apps/details?id=com.textual.chatapp",
-      //   imgUrl: "/assets/images/textual.png",
-      // },
-      {
-        title:"G-Calendar clone",
-        link:'https://csb-0m117.netlify.app/',
-        imgUrl:'/assets/images/calendar-clone.png'
-      },
-      {
-        title: "NextPortal",
-        link: "https://www.npmjs.com/package/nextportal",
-        imgUrl: "/assets/images/npm.png",
-      },
-      {
-        title: "EduWall",
-        link: "https://eduwall.in/",
-        imgUrl: "/assets/images/eduwall.png",
-      }
-    ],
-    reactProjects:[
-      {
-        title: "Todo",
-        link: "https://todo-tau-lilac.vercel.app/",
-        imgUrl: "/assets/images/todo.png",
-      },
-      {
-        title: "Delicioso",
-        link: "https://delicioso.vercel.app/",
-        imgUrl: "/assets/images/delicioso.png",
-      },
-      {
-        title: "Your quotes",
-        link: "https://todo-a1752.web.app/quotes",
-        imgUrl: "/assets/images/yourquotes.png",
-      },
-      {
-        title: "Foto factory",
-        link: "https://foto-factory-nextjs.vercel.app/",
-        imgUrl: "/assets/images/fotofactory.png",
-      },
-
-    ],
-    about: {
-      title:
-        "I'm a 22 year old software developer, mostly interested in everything web, currently working as a frontend developer at KredX.",
-      description: [
-        `My interest towards computers started back when I was in 9th grade. My journey on the internet began as a blogger, back when I used to run a tech blog writing and sharing articles on everything I learned. `,
-        `This helped me network and grow connections amongst the fraternity and mint some money as well :P. Eventually, things got serious when I got into coding and development. I've always wanted to(still do) build cool stuff that could impact/be used by lots of people.`,
-        `Initially I began developing apps and have built and published several apps for Android. Lately I've grown interest towards open source projects and often contribute to them.
-        This helps be better understand different codebases. 
-        I always take keen interest in learning new things and use the power of code to make this world better place.`,
-      ],
-      currentProject: "NA",
-      currentProjectUrl: "NA",
+  githubUsername: "prajwalkulkarni",
+  name: "Prajwal Kulkarni",
+  designation: "Frontend Developer",
+  avatarUrl: "/assets/images/avatar.jpg",
+  email: "prajwalkulkarni76@gmail.com",
+  phone: "+91 8105976325",
+  address: "Bengaluru, Karnataka, India.",
+  projects: [
+    {
+      title: "Splitster",
+      link: "https://play.google.com/store/apps/details?id=com.app.splitster&hl=en_IN&gl=US",
+      imgUrl: "/assets/images/splitster.png",
     },
-    experience: [
-      {
-        title: "Blogging",
-        company: "Bizzflies",
-        year: "2015",
-        companyLink: "https://bizzflies.blogspot.in/",
-        desc: "Ran and maintained a tech blog for over a year.",
-      },
-      {
-        title: "School",
-        company: "Kendriya Vidayala, BEML.",
-        year: "2016",
-        companyLink: "https://bemlnagarkolar.kvs.ac.in/",
-        desc: "Primary and secondary schooling. CBSE boards.",
-      },
-      {
-        title: "University",
-        company: "BMSCE, Bengaluru",
-        year: "2018-2022",
-        companyLink: "https://bmsce.ac.in",
-        desc: "Majoring in Information Science and Engineering.",
-      },
-      {
-        title: "App dev",
-        company: "Phase Shift",
-        year: "2019",
-        companyLink: "https://play.google.com/store/apps/details?id=com.bmsce.phaseshift&hl=en_IN&gl=US",
-        desc: "Volunteerd and developed mobile application for BMSCE's annual tech symposium, PhaseShift.",
-      },
-      {
-        title: "Freelancing",
-        company: "Web and app developer",
-        year: "2020",
-        companyLink: "https://fiverr.com",
-        desc: "Freelanced as a web and mobile app devloper on Fiverr for a short period of time.",
-      },
-      {
-        title: "Technical writing",
-        company: "Medium, Freecodecamp",
-        year: "2020",
-        companyLink: "https://linktr.ee/prajwalk",
-        desc: "Started writing technical articles on various platforms.",
-      },
-      {
-        title: "WWDC 2021",
-        company: "Apple",
-        year: "2021",
-        companyLink: "https://apple.com",
-        desc: "Won the WWDC scholarship event conducted by Apple.",
-      },
-
-    ],
-    resumeUrl:
-      "",
-    socialLinks: {
-      twitter: "https://twitter.com/prajwalinbizz",
-      linkedin: "https://linkedin.com/in/prajwal-kulkarni",
-      github: "https://github.com/prajwalkulkarni",
-      stackoverflow:'https://stackoverflow.com/users/12828566/prajwal-kulkarni',
-      facebook: "https://facebook.com/",
+    // {
+    //   title: "Textual",
+    //   link: "https://play.google.com/store/apps/details?id=com.textual.chatapp",
+    //   imgUrl: "/assets/images/textual.png",
+    // },
+    {
+      title: "G-Calendar clone",
+      link: "https://csb-0m117.netlify.app/",
+      imgUrl: "/assets/images/calendar-clone.png",
     },
-  };
-  
-  export default userData;
+    {
+      title: "NextPortal",
+      link: "https://www.npmjs.com/package/nextportal",
+      imgUrl: "/assets/images/npm.png",
+    },
+    {
+      title: "EduWall",
+      link: "https://eduwall.in/",
+      imgUrl: "/assets/images/eduwall.png",
+    },
+  ],
+  reactProjects: [
+    {
+      title: "Todo",
+      link: "https://todo-tau-lilac.vercel.app/",
+      imgUrl: "/assets/images/todo.png",
+    },
+    {
+      title: "Delicioso",
+      link: "https://delicioso.vercel.app/",
+      imgUrl: "/assets/images/delicioso.png",
+    },
+    {
+      title: "Your quotes",
+      link: "https://todo-a1752.web.app/quotes",
+      imgUrl: "/assets/images/yourquotes.png",
+    },
+    {
+      title: "Foto factory",
+      link: "https://foto-factory-nextjs.vercel.app/",
+      imgUrl: "/assets/images/fotofactory.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a 22 year old software developer, mostly interested in everything web, currently working as a frontend developer at KredX.",
+    description: [
+      `My interest towards computers started back when I was in 9th grade. My journey on the internet began as a blogger, back when I used to run a tech blog writing and sharing articles on everything I learned. `,
+      `This helped me network and grow connections amongst the fraternity and make some money as well, haha. Eventually, things got serious when I got into coding and development. I've always wanted to(still do) build cool stuff that could impact/be used by lots of people.`,
+      `Initially I began developing apps for Android, tried my hands-on backend and iOS development and currently pursuing frontend web development. I'm also an open source enthusiast and often contribute to it.
+        I always take keen interest in learning new things and use the power of code to make this a world better place.`,
+    ],
+    currentProject: "NA",
+    currentProjectUrl: "NA",
+  },
+  experience: [
+    {
+      title: "Blogging",
+      company: "Bizzflies",
+      year: "2015",
+      companyLink: "https://bizzflies.blogspot.in/",
+      desc: "Ran and maintained a tech blog for over a year.",
+    },
+    {
+      title: "School",
+      company: "Kendriya Vidayala, BEML.",
+      year: "2016",
+      companyLink: "https://bemlnagarkolar.kvs.ac.in/",
+      desc: "Primary and secondary schooling. CBSE boards.",
+    },
+    {
+      title: "University",
+      company: "BMSCE, Bengaluru",
+      year: "2018-2022",
+      companyLink: "https://bmsce.ac.in",
+      desc: "Majoring in Information Science and Engineering.",
+    },
+    {
+      title: "App dev",
+      company: "Phase Shift",
+      year: "2019",
+      companyLink:
+        "https://play.google.com/store/apps/details?id=com.bmsce.phaseshift&hl=en_IN&gl=US",
+      desc: "Volunteerd and developed mobile application for BMSCE's annual tech symposium, PhaseShift.",
+    },
+    {
+      title: "Freelancing",
+      company: "Web and app developer",
+      year: "2020",
+      companyLink: "https://fiverr.com",
+      desc: "Freelanced as a web and mobile app devloper on Fiverr for a short period of time.",
+    },
+    {
+      title: "Technical writing",
+      company: "Medium, Freecodecamp",
+      year: "2020",
+      companyLink: "https://linktr.ee/prajwalk",
+      desc: "Started writing technical articles on various platforms.",
+    },
+    {
+      title: "WWDC 2021",
+      company: "Apple",
+      year: "2021",
+      companyLink: "https://apple.com",
+      desc: "Won the WWDC scholarship event conducted by Apple.",
+    },
+  ],
+  resumeUrl: "",
+  socialLinks: {
+    twitter: "https://twitter.com/prajwalinbizz",
+    linkedin: "https://linkedin.com/in/prajwal-kulkarni",
+    github: "https://github.com/prajwalkulkarni",
+    stackoverflow: "https://stackoverflow.com/users/12828566/prajwal-kulkarni",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
