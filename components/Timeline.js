44ee1ee271783984cdb332709d1c6822dac61a21
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-10-29 17:19:44 +0530

    Update timeline UI and projects

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,78 +1,92 @@
 import userData from "../constants/data";
 import React from "react";
-import styles from './Timeline.module.css'
+import styles from "./Timeline.module.css";
 export default function Timeline() {
   return (
-    <section className="bg-white dark:bg-gray-800 overflow-x-hidden">
-      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left dark:text-white">
+    <section className="overflow-x-hidden bg-white dark:bg-gray-800">
+      <div className="h-48 max-w-6xl mx-auto bg-white dark:bg-gray-800">
+        <h1 className="py-20 text-5xl font-bold text-center md:text-9xl md:text-left dark:text-white">
           Timeline
         </h1>
       </div>
-      <div className="bg-[#F1F1F1] dark:bg-gray-900 ">
-        <div className={`md:flex w-screen`}>
-          {/* Experience card */}
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 flex justify-center mt-24">
+        {/* Experience card */}
+        <ol class="relative border-l border-gray-200 dark:border-gray-700">
           {userData.experience.map((exp, idx) => (
-            <div className={`md:flex flex-none m-3 ${styles.scrollBg}`} key={idx}>
-              <TimelineCard
-                key={idx}
-                title={exp.title}
-                desc={exp.desc}
-                year={exp.year}
-                company={exp.company}
-                companyLink={exp.companyLink}
-                
-              />
-              
-            </div>
+            <TimelineCard
+              key={idx}
+              title={exp.title}
+              desc={exp.desc}
+              year={exp.year}
+              company={exp.company}
+              companyLink={exp.companyLink}
+              isLatest={idx === 0}
+            />
           ))}
-        </div>
+        </ol>
       </div>
     </section>
   );
 }
 
-const TimelineCard = ({ title, desc, year, company, companyLink }) => {
-  {/*return (
-    <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
-      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
-        {year}
-      </h1>
-      <h1 className="font-semibold text-xl">{title}</h1>
-      <a href={companyLink} className="text-gray-500">
-        {company}
-      </a>
-      <p className="text-gray-600 dark:text-gray-400 my-2">{desc}</p>
-    </div>
-  );*/}
+const TimelineCard = ({
+  title,
+  desc,
+  year,
+  company,
+  companyLink,
+  isLatest = false,
+}) => {
   return (
-    <div
-      className={`md:card-container flex flex-col justify-between shadow-lg max-w-lg min-w-lg rounded-b-md rounded-t-md `}>
-      <div className="top-text p-8">
-        <div className="icon-quotes">
-          <h1 className="text-4xl text-gray-200 font-bold dark:text-gray-800">
-            {title}
-          </h1>
-          <a href={companyLink} target="_blank" rel="noreferrer" className="text-gray-500">
-            {company}
-          </a>
-        </div>
-        <div className="container-text max-w-lg">
-          <p className="font-semibold text-gray-600 dark:text-gray-400 text-xl py-8">
-            {desc}
-          </p>
-        </div>
-      </div>
-      <div className="footer-container flex flex-row flex-nowrap justify-between min-w-lg max-w-lg bg-gradient-to-r from-green-400 to-blue-500 py-4 px-8 rounded-b-lg">
-        <div className="avatar-container">
-          <div className="footer-img flex flex-row flex-nowrap">
-
-            <div className="footer-text flex flex-col flex-nowrap">
-              <p className="text-gray-100 font-bold ml-6">{year}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    <li class="mb-10 ml-6">
+      <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        <svg
+          class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+        </svg>
+      </span>
+      <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+        {title} |{" "}
+        <a
+          href={companyLink}
+          target="_blank"
+          className="text-blue-400 px-2.5 underline"
+        >
+          {company}
+        </a>
+        {isLatest && (
+          <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">
+            Latest
+          </span>
+        )}
+      </h3>
+      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+        {year}
+      </time>
+      <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+        {desc}
+      </p>
+      {/* <a
+        href="#"
+        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+      >
+        <svg
+          class="w-3.5 h-3.5 mr-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
+          <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
+        </svg>{" "}
+        Download ZIP
+      </a> */}
+    </li>
+  );
 };
