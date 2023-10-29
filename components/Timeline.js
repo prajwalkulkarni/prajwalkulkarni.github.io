4f95ec836d689f90b3223e73abf622c7923f7bee
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-10-29 21:39:27 +0530

    Update attrib to className

--- components/Timeline.js
+++ components/Timeline.js
@@ -11,7 +11,7 @@ export default function Timeline() {
       </div>
       <div className="bg-[#F1F1F1] dark:bg-gray-900 flex justify-center mt-24">
         {/* Experience card */}
-        <ol class="relative border-l border-gray-200 dark:border-gray-700">
+        <ol className="relative border-l border-gray-200 dark:border-gray-700">
           {userData.experience.map((exp, idx) => (
             <TimelineCard
               key={idx}
@@ -38,10 +38,10 @@ const TimelineCard = ({
   isLatest = false,
 }) => {
   return (
-    <li class="mb-10 ml-6">
-      <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+    <li className="mb-10 ml-6">
+      <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
         <svg
-          class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
+          className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -50,7 +50,7 @@ const TimelineCard = ({
           <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
         </svg>
       </span>
-      <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+      <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
         {title} |{" "}
         <a
           href={companyLink}
@@ -60,15 +60,15 @@ const TimelineCard = ({
           {company}
         </a>
         {isLatest && (
-          <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">
+          <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">
             Latest
           </span>
         )}
       </h3>
-      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+      <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
         {year}
       </time>
-      <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+      <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
         {desc}
       </p>
       {/* <a
