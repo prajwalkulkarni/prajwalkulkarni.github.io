835ef776f3bf75d06617798093521e54d127314c
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2026-01-04 19:38:03 +0530

    chore: change cdn uri to relative protocol

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
-  
+
   images: {
     // domains: ['raw.githubusercontent.com','ibb.co'],
-    domains: ['raw.githubusercontent.com'],
-    loader:'imgix',
-    path:''
+    domains: ["raw.githubusercontent.com"],
+    loader: "imgix",
+    path: "",
   },
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/prajwalkulkarni/prajwalkulkarni.github.io/gh-pages/' : '',
-}
+  assetPrefix: isProd
+    ? "//cdn.statically.io/gh/prajwalkulkarni/prajwalkulkarni.github.io/gh-pages/"
+    : "",
+};
